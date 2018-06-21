ac1cdf881035a14ff505fac828381bcdb59c8d82
jose.moreno <jolorenzom@gmail.com>
2018-06-21 14:13:57 +0200

    Comienzo a implementar acceso por token

--- app/controller/note.controller.js
+++ app/controller/note.controller.js
@@ -1,38 +1,56 @@
 const db = require('../config/db.config.js');
 const Note = db.notes;
 
+const tok = 'entrar';
 
-exports.indice = (req, res) => {
-	res.send('Bienvenido al NodeJS API McFly realizado por Jose Lorenzo Moreno Moreno. TOKEN:'+req.get('token'));
+var functionToken = (token) => { 
+	if(token==tok){
+		return 1; 
+	}else 
+		return 0;
 };
 
-// Post a Note
+//Como USUARIO quiero poder llamar al API, es decir, 
+//quiero poder tener un servidor local al que hacer una llamada HTTP y que me devuelva algo.
+exports.indice = (req, res) => {
+	if(functionToken(req.get('token'))==0)
+	{
+		res.send('ERROR de acceso. TOKEN incorrecto');		
+	}else{
+		res.send('Bienvenido al NodeJS API McFly realizado por Jose Lorenzo Moreno Moreno. TOKEN:'+req.get('token'));	
+	}	
+};
+
+// Como USUARIO quiero poder llamar al API para crear notas.
 exports.create = (req, res) => {	
-	// Save to MySQL database
 	Note.create({  
 	  content: req.body.content,
 	  favorite: 0
 	}).then(note => {		
-		// Send created note to client
 		res.send(note);
 	});
 };
  
-// FETCH all Notes
+// Como USUARIO quiero poder llamar al API para consultar las notas.
 exports.findAll = (req, res) => {
+	if(functionToken(req.get('token'))==0)
+	{
+		res.send('ERROR de acceso. TOKEN incorrecto');		
+	}
+
 	Note.findAll().then(notes => {
-	  // Send all notes to Client
 	  res.send(notes);
 	});
 };
 
-// Find a Note by Id
+// Como USUARIO quiero poder llamar al API para consultar una sóla nota.
 exports.findById = (req, res) => {	
 	Note.findById(req.params.noteId).then(note => {
 		res.send(note);
 	})
 };
 
+// Como USUARIO quiero poder llamar al API para consultar las notas marcadas como favoritas.
 exports.findFavs = (req, res) => {	
 	Note.findAll({
 	  where: {
@@ -43,7 +61,7 @@ exports.findFavs = (req, res) => {
 		});
 };
  
-// Update a Note
+// Como USUARIO quiero poder llamar al API para marcar favorita una nota.
 exports.update = (req, res) => {
 	const id = req.params.noteId;
 	Note.update( { favorite: 1 }, 
--- app/route/note.route.js
+++ app/route/note.route.js
@@ -3,20 +3,22 @@ module.exports = function(app) {
  
     const notes = require('../controller/note.controller.js');
     
+    // Como USUARIO quiero poder llamar al API, es decir, 
+    //quiero poder tener un servidor local al que hacer una llamada HTTP y que me devuelva algo.
     app.get('/apimcfly', notes.indice);
 
-    // Create a new Notes
+    // Como USUARIO quiero poder llamar al API para crear notas.
     app.post('/apimcfly/notes', notes.create);
  
-    // Retrieve all Notes
+    // Como USUARIO quiero poder llamar al API para consultar las notas.
     app.get('/apimcfly/notes', notes.findAll);
  
-    // Retrieve a single Note by Id
+    // Como USUARIO quiero poder llamar al API para consultar una sola nota.
     app.get('/apimcfly/notes/:noteId', notes.findById);
 
-    // Retrieve all favorites Notes
+    // Como USUARIO quiero poder llamar al API para consultar las notas marcadas como favoritas.
     app.get('/apimcfly/notes-favs', notes.findFavs);
  
-    // Update a Note with Id
+    // Como USUARIO quiero poder llamar al API para marcar favorita una nota.
     app.put('/apimcfly/notes/:noteId', notes.update);
 }
